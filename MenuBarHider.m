1a1166d87ef8ed1b38805d3f3db06c5002ef7417
Crazor <crazor@gmail.com>
2010-10-05 13:03:05 +0200

    Added define for DEBUG to Xcode's Debug target. Added observation code for NSApplication's currentSystemPresentationMode fixes #1 for now.

--- MenuBarHider.m
+++ MenuBarHider.m
@@ -4,9 +4,60 @@
 
 @implementation MenuBarHider
 
+static MenuBarHider *sharedInstance;
+
 + (void)load
 {
+#ifdef DEBUG
+	NSLog(@"MenuBarHider loaded.");
+#endif
 	SetSystemUIMode(kUIModeAllSuppressed, 0);
+	[[NSApplication sharedApplication] addObserver:[MenuBarHider sharedInstance]
+										forKeyPath:@"currentSystemPresentationOptions"
+										   options:(NSKeyValueObservingOptionNew
+													| NSKeyValueObservingOptionOld)
+										   context:NULL];
+}
+
++ (void)initialize
+{
+	static BOOL initialized = NO;
+	if (!initialized)
+	{
+		sharedInstance = [[MenuBarHider alloc] init];
+		initialized = YES;
+	}
+}
+
++ (MenuBarHider *)sharedInstance
+{
+	return sharedInstance;
+}
+
+- (id)init
+{
+	if (sharedInstance)
+	{
+		[self dealloc];
+		return sharedInstance;
+	}
+	
+	return self;
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath
+					  ofObject:(id)object
+						change:(NSDictionary *)change
+					   context:(void *)context
+{
+	if ([[NSApplication sharedApplication] isActive]
+		&&[[change objectForKey:@"new"] integerValue] == 0)
+	{
+#ifdef DEBUG
+		NSLog(@"We need to change SystemUIMode back to auto-hide the menu bar.");
+#endif
+		SetSystemUIMode(kUIModeAllSuppressed, 0);
+	}
 }
 
 @end
