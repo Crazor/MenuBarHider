01a5cda23c6b50228cf643aa5d23efaa897be871
Crazor <crazor@gmail.com>
2014-04-15 15:56:21 +0200

    Add copyright and licensing notice.

--- MenuBarHider.m
+++ MenuBarHider.m
@@ -1,7 +1,27 @@
+/*
+ * This file is part of the MenuBarHider project.
+ *
+ * Copyright 2010-2014 Crazor <crazor@gmail.com>
+ *
+ * MenuBarHider is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MenuBarHider is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MenuBarHider.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #import "MenuBarHider.h"
 
 #import "JRSwizzle/JRSwizzle.h"
 #import <Carbon/Carbon.h>
+
 
 @interface NSScreen (MenuBarHider)
 - (NSRect)myVisibleFrame;
@@ -13,6 +33,7 @@
     return [self frame];
 }
 @end
+
 
 @implementation MenuBarHider
 
